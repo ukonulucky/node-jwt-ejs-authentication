b097929cd4ec96f545a16187eae48aab02efa90c
ukonulucky <ukonulucky@gmail.com>
2022-08-10 01:18:05 +0000

    jwt-auth

---
+++ controlers/authControlers.js
@@ -0,0 +1,17 @@
+const signup_get = (req,res) => {
+
+}
+
+const signup_post = (req,res) => {
+
+}
+
+const login_post = (req,res) => {
+
+}
+
+const login_get = (req,res) => {
+
+}
+
+module.exports = {signup_get, signup_post, login_get, login_post}
---
+++ routes/router.js
@@ -0,0 +1,24 @@
+const { Router } = require("express")
+const { signup_get, signup_post, login_get, login_post} = require("../controlers/authControlers")
+
+const router = Router()
+
+
+// login post rout
+
+ router.post("/login", login_post)
+
+// login get route
+
+  router.get("/login", login_get)
+//signup post route
+
+router.post("signup", signup_post)
+
+// signup get route
+
+router.get("/signup", signup_get)
+
+
+
+module.exports = router
